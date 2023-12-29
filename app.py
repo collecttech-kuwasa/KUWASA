94f265384b4131d58a85e32811bb4b35c150cc3f
collecttech-kuwasa <154560730+collecttech-kuwasa@users.noreply.github.com>
2023-12-29 06:37:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def index():
     cursor = connection.cursor()
 
     cursor.execute("""
-        SELECT customername, accountnumber, controlnumber, meternumber, mobilenumber, house_number, zonename, routename, resident_type,street_name,
+        SELECT customername, accountnumber, controlnumber, meternumber, mobilenumber, house_number, zonename, routename, resident_type,
             ST_Y(ST_Transform(geom, 4326)) AS latitude_column,  
             ST_X(ST_Transform(geom, 4326)) AS longitude_column 
         FROM gis.customers_debts_with_id;
@@ -50,7 +50,6 @@ def index():
         'resident_type': customer[8],
         'latitude': float(customer[9]) if customer[9] is not None else None,
         'longitude': float(customer[10]) if customer[10] is not None else None,
-		'street_name': customer[11] if customer[11] is not None else None,
     } for customer in customers]
 
     # Use json.dumps to serialize the customer data
@@ -64,7 +63,7 @@ def customer_details(account_number):
     cursor = connection.cursor()
 
     query = """
-        SELECT customername, accountnumber, controlnumber, meternumber, mobilenumber, house_number, zonename, routename, resident_type,street_name,
+        SELECT customername, accountnumber, controlnumber, meternumber, mobilenumber, house_number, zonename, routename, resident_type,
             ST_Y(ST_Transform(geom, 4326)) AS latitude_column,  
             ST_X(ST_Transform(geom, 4326)) AS longitude_column 
         FROM gis.customers_debts_with_id
